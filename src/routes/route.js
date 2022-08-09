77d076c41910d7cc116f8c043732e4503846017e
Dharm-8434 <dharm15121998@gmail.com>
2022-08-09 23:16:22 +0530

    not complete

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,20 +1,21 @@
 const express = require('express');
+const lodash = require('lodash');
 const abc = require('../introduction/intro')
 const wel3 = require('../logeer/loger.js')
 const abcd = require('../util/helper');
 const router = express.Router();
 const abcd2 = require('../util/helper')
 
+
 router.get('/test-me', function (req, res) {
     console.log('My batch is', abc.name)
     console.log(wel3.stu,abcd.dat);
-    
+    console.log(resul);
     wel3.welcome_massage()
     abc.printName()
     abcd.re()
     abcd.batch()
     abcd2.resu
-
 
     res.send('My second ever api!')
 });
