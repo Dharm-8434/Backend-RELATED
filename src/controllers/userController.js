778a12b5825d2c5c0ce20be5ce0896713e7a8fbe
Dharm-8434 <dharm15121998@gmail.com>
2022-08-17 17:50:37 +0530

    aor

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -8,10 +8,8 @@ const createUser= async function (req, res) {
 
 const getUsersData= async function (req, res) {
     //let year = req.year;
-    let allUsers= await UserModel.find({$or: [{authorName:"Dharm"}].select({bookname:1, authorName:1})})
-    
-   res.send({msg: allUsers})
+    let allUsers= await UserModel.find().select({bookName:1, authorName:1})
+    res.send({msg:allUsers})
 }
-
 module.exports.createUser= createUser
 module.exports.getUsersData= getUsersData
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const moment = require('moment');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
