2988acd1c87163ef2189307bb19bc58c54e8f21b
Dharm-8434 <dharm15121998@gmail.com>
2022-08-26 17:07:24 +0530

    hda

--- src/index.js
+++ src/index.js
@@ -7,15 +7,23 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Dharm843418:W7925PTcF6A0Tpmb@cluster0.aspg5fr.mongodb.net/Dharm8434?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
+
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
 
-app.use('/', route)
+
+app.use('/', route);
+
+app.use (
+    function (req, res, next) {
+        console.log ("inside GLOBAL MW");
+        res.send({msg:"done"})
+  }
+  );
 
 
 app.listen(process.env.PORT || 3000, function () {
